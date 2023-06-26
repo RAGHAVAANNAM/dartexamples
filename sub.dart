0aac9154af42d534f60524aafb32789f87a1748f
raghava annam <raghavaannam9@gmail.com>
2023-06-26 16:12:17 +0530

    Substraction

---
+++ sub.dart
@@ -0,0 +1,23 @@
+void main() {
+  int a = 1244;
+  int b = 3244;
+  int c = a + b;
+  String q = "munipa";
+  String w = "lle";
+  int d = 565;
+  int g = c - d;
+  {
+    int f = 5;
+    int j = (f * g);
+    print("multiple of j= ${f * g}");
+    int t = 1000;
+    int o = t + a;
+
+    print("value of O= ${t + a}");
+  }
+
+  print(q + w);
+  print("addition od c=${a + b}");
+
+  print("substr g=${d - c}");
+}
