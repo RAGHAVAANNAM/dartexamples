d224166d472d87f630169b146aefdb35e27692ea
raghava annam <raghavaannam9@gmail.com>
2023-06-26 16:40:12 +0530

    multiplication

---
+++ mul.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 66778;
+  int r = 57866;
+  String x = "sun";
+  String y = "Rise";
+
+  double z = 22.56;
+
+  int b = a + r;
+  var c = z + b;
+  String w = x + y;
+
+  print("adition of b =${a + r}");
+  print(x + y);
+  print("double c =${z + b}");
+}
