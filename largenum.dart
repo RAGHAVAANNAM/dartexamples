61cbd41cafb17039c7499e1b91b3d89cd8c6ab93
raghava annam <raghavaannam9@gmail.com>
2023-07-07 16:00:10 +0530

    find large number small number

---
+++ largenum.dart
@@ -0,0 +1,35 @@
+void main() {
+  var list = [12, 45, 23, 76, 98, 44];
+
+  print(list);
+  var big_value = list[0];
+  var small_value = list[0];
+
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] > big_value) {
+      big_value = list[i];
+    }
+    if (list[i] < small_value) {
+      small_value = list[i];
+    }
+  }
+
+  print("big_valuein the list:${big_value}");
+  print("small_valuein the list: ${small_value}");
+
+  list.addAll([99, 120, 345, 3, 7656]);
+  {
+    print(list);
+    for (var i = 0; i < list.length; i++) {
+      if (list[i] > big_value) {
+        big_value = list[i];
+      }
+      if (list[i] < small_value) {
+        small_value = list[i];
+      }
+    }
+
+    print("big_valuein the list:${big_value}");
+    print("small_valuein the list: ${small_value}");
+  }
+}
